090ee985dc11014d6a910352d6d2f545c8c21e32
umerrazzaq786 <engumergujjar786@gmail.com>
2025-06-05 15:38:47 +0500

    Add hello page or hello route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return view('hello');
+// Route::get('/hello', function () {
+//     return view('hello');
+// });
+
+// Route::get('/about',function():{
+//     return view('about');
+// });
+Route::get('/hello', function(){
+    return view ('hello');
 });
